a0d9bdcf6905f7bd29c7db238b7ff8d82f834786
asaminiko <chebakova29@gmail.com>
2025-02-21 13:29:11 +0200

    Updated task 2

--- task2_threeBtns/script.js
+++ task2_threeBtns/script.js
@@ -1,10 +1,5 @@
 const parent = document.getElementById('parent')
+let btns = document.getElementsByTagName('button')
 parent.addEventListener('click', function (event) {
-  if (event.target.id == 'btn1') {
-    alert('Кнопка 1')
-  } else if (event.target.id == 'btn2') {
-    alert('Кнопка 2')
-  } else if (event.target.id == 'btn3') {
-    alert('Кнопка 3')
-  }
+  if (event.target.tagName === 'BUTTON') alert(event.target.textContent)
 })
